ac7faac8e981fc3ca37c02a083b261056ca8d25c
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-02-19 11:44:15 +0100

    Initialized photo and pokemon models.

--- PokemonApi/Models/Photo.cs
+++ PokemonApi/Models/Photo.cs
@@ -2,5 +2,12 @@
 {
     public class Photo
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly CreatedDate { get; set; }
+        public string? Author { get; set; }
+
+        public string? ImageUrl { get; set; }
     }
 }
--- PokemonApi/Models/Pokemon.cs
+++ PokemonApi/Models/Pokemon.cs
@@ -2,5 +2,11 @@
 {
     public class Pokemon
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Power { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Colour { get; set; }
     }
 }
