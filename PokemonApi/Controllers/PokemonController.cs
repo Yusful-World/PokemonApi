f98262da320ed52c036b4af06e2170458830473a
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-02-19 11:56:11 +0100

    feat: seeded tables to database.

--- PokemonApi/Controllers/PokemonController.cs
+++ PokemonApi/Controllers/PokemonController.cs
@@ -4,5 +4,6 @@ namespace PokemonApi.Controllers
 {
     public class PokemonController : ControllerBase
     {
+
     }
 }
--- PokemonApi/Data/AppDbContext.cs
+++ PokemonApi/Data/AppDbContext.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PokemonApi.Models;
 
 namespace PokemonApi.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Pokemon> Pokemons { get; set; } 
+        public DbSet<Photo> Photos { get; set; }
     }
 }
